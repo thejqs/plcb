49bc6074102f771603d5b16d8e610f7bd2a0174d
Jacob Sanders <j.q.sanders@gmail.com>
2016-03-20 15:05:58 -0400

    chunked out the data acquisition and cleaning into tighter and more testable functions

--- main/plcb_getter.py
+++ main/plcb_getter.py
@@ -7,6 +7,9 @@ import json
 
 
 def open_url(url):
+    '''
+    collects our response object
+    '''
     r = requests.get(url)
     if r.status_code == 200:
         return r.text # r.content if we need binary
@@ -30,25 +33,70 @@ def treeify(url):
     return parse_html(html)
 
 
-def dict_builder(url):
-    tree = treeify(url)
-
-    # will store data about each PLCB retail location; store_id as key
-    # stores = []
-    # will give us a string, just so we have an idea we've hit the right thing
-    num_stores_xpath = '/html/body/div[1]/div/div[3]/div[4]/div[5]/div/span/text()'
-    print tree.xpath(num_stores_xpath)
-
+def get_retail_ids(tree):
+    '''
+    each PLCB retail location has a unique store number
+    '''
     print "collecting IDs like a bouncer ..."
-    retail_store_ids = [num.strip() for num in tree.xpath('/html/body/div[1]/div/div[3]/div[4]/div/div[2]/span[2]/text()')]
+    return [num.strip() for num in tree.xpath('/html/body/div[1]/div/div[3]/div[4]/div/div[2]/span[2]/text()')]
 
+
+def get_retail_hours(tree):
+    '''
+    hours of operation come off the page a little funky, so days and hours
+    need to be collected, then turned into full weeks
+    '''
     print "getting hours ...."
     retail_store_hours = tree.xpath('/html/body/div[1]/div/div[3]/div[4]/div/div[4]/div/div/text()')
     # makes one big list of tuples of (day_of_week, hours_of_operation)
     hours_tuples = zip(retail_store_hours, retail_store_hours[1::])[::2]
     # chunks hours_tuples out into lists of tuples, each representing a week
     # and the regular hours of each store
-    hours_sets = [hours_tuples[x:x + 7] for x in xrange(0, len(hours_tuples), 7)]
+    return [hours_tuples[x:x + 7] for x in xrange(0, len(hours_tuples), 7)]
+
+
+def assemble_lat_long_address_phone(tree):
+    '''
+    the cleanest place on the page to collect these, though we'll need to break
+    them apart in short order to use them differently
+    '''
+    print "demystifying store location data ...."
+    return [option.values()[2] for option in tree.xpath('/html/body/div[1]/div/div[3]/div[4]/div/div[5]/form/input') if '<br>' not in option.values()[2]]
+
+
+def get_lat_long(tree, retail_store_data):
+    '''
+    makes sure we store these together for later mapping to an object
+    '''
+    # we'll use latitude and longitude together, and differently than address and phone. so let's chunk those out into usable storage.
+    return [tuple(retail_store_data[x:x + 2]) for x in xrange(0, len(retail_store_data), 4)]
+
+
+def get_address_phone(tree, retail_store_data):
+    '''
+    stores these together for later display in predictable order
+    '''
+    return [tuple(retail_store_data[x:x + 2]) for x in xrange(2, len(retail_store_data), 4)]
+
+
+def dict_builder(url):
+    '''
+    assembles our objects into serializable form
+    '''
+    tree = treeify(url)
+    # will give us a string, just so we have an idea we've hit the right thing
+    num_stores_xpath = '/html/body/div[1]/div/div[3]/div[4]/div[5]/div/span/text()'
+    print tree.xpath(num_stores_xpath)
+
+    retail_store_ids = get_retail_ids(tree)
+    hours_sets = get_retail_hours(tree)
+    retail_store_data = assemble_lat_long_address_phone(tree)
+    lat_long = get_lat_long(tree, retail_store_data)
+    address_phone = get_address_phone(tree, retail_store_data)
+
+    print "building serializable data sets ...."
+    # creates a list of dictionaries for serializing into json
+    return [{"id": store, "hours": hours_sets[i], "latitude": float(lat_long[i][0]), "longitude": float(lat_long[i][1]), "address": address_phone[i][0], "phone": address_phone[i][1]} for i, store in enumerate(retail_store_ids)]
 
     # print "finding store types ...."
     # need to investigate why this is giving me a weird number of values --
@@ -58,28 +106,24 @@ def dict_builder(url):
 
     # print "there are", len(retail_store_type_list), "things in the type_list"
 
-    print "demystifying store location data ...."
-    retail_store_data = [option.values()[2] for option in tree.xpath('/html/body/div[1]/div/div[3]/div[4]/div/div[5]/form/input') if '<br>' not in option.values()[2]]
-    # we'll use latitude and longitude together, and differently than address and phone. so let's chunk those out into usable storage.
-    lat_long = [tuple(retail_store_data[x:x + 2]) for x in xrange(0, len(retail_store_data), 4)]
-    address_phone = [tuple(retail_store_data[x:x + 2]) for x in xrange(2, len(retail_store_data), 4)]
 
-    print "building serializable data sets ...."
-    # creates a list of dictionaries for serializing into json
-    data = [{"id": store, "hours": hours_sets[i], "latitude": float(lat_long[i][0]), "longitude": float(lat_long[i][1]), "address": address_phone[i][0], "phone": address_phone[i][1]} for i, store in enumerate(retail_store_ids)]
-
+def write_json_to_file(data):
+    '''
+    stores our objects in a pprint format
+    '''
     print 'writing json ....'
     j = json.dumps(data, indent=4)
     with open('retail_location_data.json', 'w') as f:
         print >> f, j
 
+
+def start_scrape():
+    '''
+    runs the functions to do the damn thang
+    '''
+    data = dict_builder('http://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/FindStoreView?storeId=10051&langId=-1&catalogId=10051&pageNum=1&listSize=700&category=&city=&zip_code=&county=All+Stores&storeNO=%')
+    write_json_to_file(data)
     print "done."
 
-    return data
 
-
-# def start_scrape():
-#     '''
-#     runs the functions to do the damn thang
-#     '''
-dict_builder('http://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/FindStoreView?storeId=10051&langId=-1&catalogId=10051&pageNum=1&listSize=700&category=&city=&zip_code=&county=All+Stores&storeNO=%')
+start_scrape()
