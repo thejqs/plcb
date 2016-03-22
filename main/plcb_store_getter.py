f0790bae6d9ae19f692b2b85c1f1c86272363e34
mrstevegross <mrstevegross@gmail.com>
2016-03-22 15:44:34 -0400

    Update plcb_store_getter.py

--- main/plcb_store_getter.py
+++ main/plcb_store_getter.py
@@ -78,18 +78,26 @@ def get_retail_hours(tree):
 def unpack_lat_long_address_phone(tree):
     '''
     the cleanest place on the page to collect these and separate them
-    into their own discrete lists for later use
+    into their own lists
     '''
     print "demystifying store location data ...."
-    lat_long_address_phone_selectors = CSSSelector('.columnDistance form input')
-    lat_long_address_phone_elements = lat_long_address_phone_selectors(tree)
-
-    longitudes = [float(lat_long_address_phone_elements[x].value) for x in xrange(0, len(lat_long_address_phone_elements), 5)]
-    latitudes = [float(lat_long_address_phone_elements[x].value) for x in xrange(1, len(lat_long_address_phone_elements), 5)]
-    addresses = [lat_long_address_phone_elements[x].value for x in xrange(2, len(lat_long_address_phone_elements), 5)]
-    phone_numbers = [lat_long_address_phone_elements[x].value for x in xrange(3, len(lat_long_address_phone_elements), 5)]
-
-    return longitudes, latitudes, addresses, phone_numbers
+    inputs = CSSSelector('.columnDistance form input')(tree)
+    Entry = collections.namedtuple('Entry', 'longitude latitude address phone')
+    longitude_offset = 0
+    latitude_offset = 1
+    address_offset = 2
+    phone_offset = 3
+    input_field_count = 5
+    entries = [
+        Entry(longitude=inputs[n + longitude_offset].value,
+              latitude=inputs[n + latitude_offset].value,
+              address=inputs[n + address_offset].value,
+              phone=inputs[n + phone_offset].value)
+        for n in range(0, len(inputs), input_field_count]
+    return ([e.longitude for e in entries],
+            [e.latitude for e in entries],
+            [e.address for e in entries],
+            [e.phone for e in entries])
 
 
 def dict_builder(url):
@@ -131,10 +139,7 @@ def start_scrape():
     '''
     runs the main functions to do the damn thang
     '''
-    # a note about the URL: the site supports my manually adding a large(r)
-    # number to the listSize attribute here even though it's not a page option.
-    # the call to .format() here is simply to make that more explicit
-    data = dict_builder('http://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/FindStoreView?storeId=10051&langId=-1&catalogId=10051&pageNum=1&listSize={}&category=&city=&zip_code=&county=All+Stores&storeNO=%'.format(700))
+    data = dict_builder('http://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/FindStoreView?storeId=10051&langId=-1&catalogId=10051&pageNum=1&listSize=700&category=&city=&zip_code=&county=All+Stores&storeNO=%')
     write_json_to_file(data)
     print "done."
 
