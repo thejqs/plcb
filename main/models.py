8bf8d902502d251a4edc3130761c1996e637bab2
Jacob Sanders <j.q.sanders@gmail.com>
2016-04-04 15:40:44 -0400

    defined initial models

--- main/models.py
+++ main/models.py
@@ -7,31 +7,38 @@ class Stores(Models.model):
     store_id = models.CharField(max_length=10, primary_key=True,)
     address = models.CharField(max_length=255, null=False, blank=False)
     hours = models.CharField(max_length=255, null=True, blank=True)
-    latitude = models.IntegerField
-    longitude = models.IntegerField
-    phone = models.CharField(max_length=15, null=, blank=True)
-    is_premium_collection = models.BooleanField
+    latitude = models.FloatField
+    longitude = models.FloatField
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    store_type = models.CharField(max_length=100, null=True, blank=True)
 
     def __unicode__(self):
         return '{}'.format(Store.store_id)
 
 
-class PLCBunicorns(Models.model):
+class Unicorns(Models.model):
+    product_id = models.CharField(max_length=10)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    num_bottles = models.IntegerField
+    bottle_size = models.CharField(max_length=10, null=True, blank=True)
+    price = models.FloatField
+    on_sale = models.BooleanField(null=True, blank=True)
+    on_sale_price = models.FloatField(null=True, blank=True)
+    scrape_date = models.DateField
+    store_id = models.ForeignKey(Stores)
 
     def __unicode__(self):
         return '{}'.format(XXX)
 
 
-class Bottles(Models.model):
-
-    def __unicode__(self):
-        return '{}'.format(XXX)
-
-
-class BoozeTypes(Models.model):
-    name = models.CharField(max_length=255)
-    
-
-
-    def __unicode__(self):
-        return '{}'.format(XXX)
+# class Bottles(Models.model):
+#
+#     def __unicode__(self):
+#         return '{}'.format(XXX)
+#
+#
+# class BoozeTypes(Models.model):
+#     name = models.CharField(max_length=255)
+#
+#     def __unicode__(self):
+#         return '{}'.format(XXX)
