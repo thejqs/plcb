3192fbd5dba19557ff20e52228d1ec6518ff98a5
Jacob Sanders <j.q.sanders@gmail.com>
2016-04-07 14:15:43 -0400

    thinkin' 'bout a database.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from main.models import Stores, Unicorns
 
 # Register your models here.
+admin.site.register(Stores)
+admin.site.register(Unicorns)
--- main/models.py
+++ main/models.py
@@ -3,32 +3,35 @@ from django.db import models
 # Create your models here.
 
 
-class Stores(Models.model):
+class Stores(models.Model):
     store_id = models.CharField(max_length=10, primary_key=True,)
     address = models.CharField(max_length=255, null=False, blank=False)
     hours = models.CharField(max_length=255, null=True, blank=True)
-    latitude = models.FloatField
-    longitude = models.FloatField
+    latitude = models.FloatField()
+    longitude = models.FloatField()
     phone = models.CharField(max_length=15, null=True, blank=True)
     store_type = models.CharField(max_length=100, null=True, blank=True)
+    # county =
 
     def __unicode__(self):
-        return '{}'.format(Store.store_id)
+        return '{}'.format(self.store_id)
 
 
-class Unicorns(Models.model):
+class Unicorns(models.Model):
     product_id = models.CharField(max_length=10)
     name = models.CharField(max_length=255, null=True, blank=True)
-    num_bottles = models.IntegerField
+    num_bottles = models.IntegerField()
     bottle_size = models.CharField(max_length=10, null=True, blank=True)
-    price = models.FloatField
+    price = models.FloatField(null=True, blank=True)
     on_sale = models.BooleanField(null=True, blank=True)
     on_sale_price = models.FloatField(null=True, blank=True)
-    scrape_date = models.DateField
+    scrape_date = models.DateField()
     store_id = models.ForeignKey(Stores)
+    # proof =
+    # booze_type =
 
     def __unicode__(self):
-        return '{}'.format(XXX)
+        return '{}'.format(self.name)
 
 
 # class Bottles(Models.model):
