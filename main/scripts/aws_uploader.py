ffcc008cdc529798455de65cddd930802718e256
Jacob Sanders <j.q.sanders@gmail.com>
2016-04-27 15:15:36 -0400

    added regex to better handle dates in filenames

--- main/scripts/aws_uploader.py
+++ main/scripts/aws_uploader.py
@@ -8,6 +8,7 @@ while keeping a local copy as a backup.
 # after pip install boto3 and pip install awscli +
 # credentialing on aws with an IAM access key:
 import boto3
+import re
 import datetime
 
 
@@ -21,9 +22,14 @@ def send_to_s3(filepath):
     Args:
     a path to the local JSON file to copy
     '''
+    # makes the function more versatile because it doesn't simply rely
+    # on today's date to create the s3 filename, ensuring instead it
+    # matches whichever of our files gets handed to it
+    file_date_pattern = '((?<=\-)\d+.*(?=\.))'
+    file_date = re.search(file_date_pattern, filepath).group()
     s3 = boto3.resource('s3')
     f = open(filepath, 'rb')
-    k = s3.Bucket('boozicorns').put_object(Key='unicorns-{}.json'.format(datetime.date.today()),
+    k = s3.Bucket('boozicorns').put_object(Key='unicorns-{}.json'.format(file_date),
                                            Body=f,
                                            Metadata={'Content-Type': 'application/json'})
     k.Acl().put(ACL='public-read')
