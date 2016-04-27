4654a86e0a67a1f995367d333808a91d4438a470
Jacob Sanders <j.q.sanders@gmail.com>
2016-04-27 15:35:48 -0400

    added potential use case to main docstring. partially to remind myself later

--- main/scripts/aws_uploader.py
+++ main/scripts/aws_uploader.py
@@ -3,14 +3,27 @@
 '''
 Uploads our unicorns JSON file to an Amazon S3 bucket
 while keeping a local copy as a backup.
+
+The regex, rather than the earlier use of datetime, makes it easier
+to use this on a batch of files the way filenames are structured
+for this project. At some point I'm probably going to need to also run this
+to restore broken data or populate a new bucket. I mean, Let's be real.
+
+for example, could be used on a full directory as a module:
+import os
+import re
+import boto3
+from aws_uploader import send_to_s3
+
+for i in os.listdir('../data/unicorns_json'):
+    send_to_s3('../data/unicorns_json/' + i)
+
 '''
 
 # after pip install boto3 and pip install awscli +
 # credentialing on aws with an IAM access key:
 import boto3
 import re
-import datetime
-
 
 def send_to_s3(filepath):
     '''
@@ -24,7 +37,8 @@ def send_to_s3(filepath):
     '''
     # makes the function more versatile because it doesn't simply rely
     # on today's date to create the s3 filename, ensuring instead it
-    # matches whichever of our files gets handed to it
+    # matches whichever of our files gets handed to it.
+    # this makes it easier to run while looping through a directory.
     file_date_pattern = '((?<=\-)\d+.*(?=\.))'
     file_date = re.search(file_date_pattern, filepath).group()
     s3 = boto3.resource('s3')
