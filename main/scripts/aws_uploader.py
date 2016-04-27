b400930d2d080083afcc13fd19b77aa4f8c523a7
Jacob Sanders <j.q.sanders@gmail.com>
2016-04-27 12:16:56 -0400

    updated docstrings

--- main/scripts/aws_uploader.py
+++ main/scripts/aws_uploader.py
@@ -1,12 +1,26 @@
 #!usr/bin/env python
 
+'''
+Uploads our unicorns JSON file to an Amazon S3 bucket
+while keeping a local copy as a backup.
+'''
+
 # after pip install boto3 and pip install awscli +
-# credentialing done on aws with an IAM access key:
+# credentialing on aws with an IAM access key:
 import boto3
 import datetime
 
 
 def send_to_s3(filepath):
+    '''
+    called from pdf_multi_plcb_product_getter.py
+
+    uploads a copy of the file, setting permissions and metadata
+    for immediate access
+
+    Args:
+    a path to the local JSON file to copy
+    '''
     s3 = boto3.resource('s3')
     f = open(filepath, 'rb')
     k = s3.Bucket('boozicorns').put_object(Key='unicorns-{}.json'.format(datetime.date.today()),
