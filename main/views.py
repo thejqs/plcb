20bd794578ebbe1f42d95cd0271d65d32863fe5c
Jacob Sanders <j.q.sanders@gmail.com>
2016-05-09 22:58:03 -0400

    search results appearing, preparing to style

--- main/views.py
+++ main/views.py
@@ -50,14 +50,14 @@ class UnicornView(View):
         return sorted(lst, key=lambda k: k.price, reverse=True)
 
     def find_top_store(self, lst):
-        """
+        '''
         given a list, finds the number of occurrences of each item, returning
         the most-common value
-        """
+        '''
         return max([(x, lst.count(x)) for x in lst], key=itemgetter(1))
 
     def top_10_price_minus_sale_price(self, lst_of_objs):
-        """
+        '''
         given a list of Django objects, each of which contains 'price' and
         'on_sale_price' attributes, finds the largest gaps between retail
         price and sale price -- in other words, the most deeply discounted
@@ -65,12 +65,12 @@ class UnicornView(View):
 
         Returns:
         the top 10
-        """
+        '''
         s = (c for c in lst_of_objs if c.on_sale_price > 0)
         return sorted(s, key=lambda k: k.price - k.on_sale_price, reverse=True)[:10]
 
     def top_10_percent_discount(self, lst_of_objs):
-        """
+        '''
         given a list of Django objects, each of which contains 'price' and
         'on_sale_price' attributes, finds the largest percentage gaps between
         retail price and sale price for products retailing for at least $20 --
@@ -79,7 +79,7 @@ class UnicornView(View):
 
         Returns:
         the top 10
-        """
+        '''
         s = (c for c in lst_of_objs if c.on_sale_price > 0 and c.price > 20)
         return sorted(s, key=lambda k: 1 - (k.on_sale_price / k.price), reverse=True)[:10]
 
@@ -199,12 +199,6 @@ class UnicornView(View):
 
     def post(self, request):
         context = {}
-        unicorns_dict = {}
-
-        boozicorns = Unicorns.objects.filter(scrape_date=today.strftime('%Y-%m-%d'))
-        if not boozicorns:
-            boozicorns = Unicorns.objects.filter(scrape_date=yesterday.strftime('%Y-%m-%d'))
-
         form = SearchBoozicornForm(request.POST)
         context['form'] = form
 
@@ -214,91 +208,11 @@ class UnicornView(View):
             context['unicorn_response'] = Unicorns.objects.filter(name__icontains=search).filter(scrape_date=today.strftime('%Y-%m-%d'))
             if not context['unicorn_response']:
                 context['unicorn_response'] = Unicorns.objects.filter(name__icontains=search).filter(scrape_date=yesterday.strftime('%Y-%m-%d'))
-            context['message'] = "WOO BOOZICORNS"
+            if context['unicorn_response']:
+                context['message'] = "WOO BOOZICORNS"
+            else:
+                context['message'] = "Ain't got none of them, hoss."
         else:
             context['message'] = form.non_field_errors
 
-        max_price = None
-        min_price = None
-        min_name = None
-        max_name = None
-        most_bottles = None
-        most_bottles_price = None
-        most_bottles_on_sale = None
-        most_bottles_name = None
-        most_bottles_store_id = None
-        stores = []
-        whiskey = []
-        rum = []
-        agave = []
-        gin = []
-        fancy = []
-
-        all_prices = [u.price for u in boozicorns]
-        most_common_price = Counter(all_prices).most_common()[0]
-        median_price = self.find_median(all_prices)
-
-        # capturing various summary data in one loop through the JSON objects
-        for unicorn in boozicorns:
-            name = unicorn.name
-            num_bottles = int(unicorn.num_bottles)
-            price = unicorn.price
-            on_sale = unicorn.on_sale_price
-            # we'll count these later to see which store has the most
-            stores.append(unicorn.store_id)
-
-            if price > 100:
-                fancy.append(unicorn)
-
-            if 'Whiskey' in name and unicorn not in whiskey:
-                whiskey.append(unicorn)
-            if 'Bourbon' in name and unicorn not in whiskey:
-                whiskey.append(unicorn)
-            if 'Scotch' in name and unicorn not in whiskey:
-                whiskey.append(unicorn)
-            if 'Rum' in name and unicorn not in rum:
-                rum.append(unicorn)
-            if 'Tequila' in name and unicorn not in agave:
-                agave.append(unicorn)
-            if 'Mezcal' in name and unicorn not in agave:
-                agave.append(unicorn)
-            if 'Gin' in name and 'Ginjo' not in name and 'Ginger' not in name and unicorn not in gin:
-                gin.append(unicorn)
-
-            if (min_price is None or min_price > price) and price > 1 and unicorn.bottle_size != 'EACH':
-                min_price = price
-                min_name = name
-            if most_bottles is None or most_bottles < num_bottles:
-                most_bottles = num_bottles
-                most_bottles_name = name
-                most_bottles_price = price
-                most_bottles_on_sale = 'Sale price: ${}'.format(unicorn.on_sale_price) if unicorn.on_sale_price else None
-                most_bottles_store_id = unicorn.store_id
-
-        top_store = self.find_top_store(stores)
-        top_store_contents = [unicorn for unicorn in boozicorns if unicorn.store_id == top_store[0]]
-
-        # formatted thus to work with a JavaScript function and Ajax call to set the map
-        unicorns_dict['scrape_date'] = str(boozicorns[0].scrape_date)
-        # formatted thus for display
-        unicorns_dict['data_date'] = '{}'.format(boozicorns[0].scrape_date.strftime('%d %B %Y'))
-        unicorns_dict['discounted'] = self.top_10_price_minus_sale_price(boozicorns)
-        unicorns_dict['percent_discount'] = self.top_10_percent_discount(boozicorns)
-        unicorns_dict['min'] = [min_name.lower(), '${}'.format(min_price)]
-        unicorns_dict['mode'] = most_common_price
-        unicorns_dict['median'] = median_price
-        unicorns_dict['bottles'] = [most_bottles_name.lower(), most_bottles, '${}'.format(most_bottles_price), most_bottles_on_sale]
-        unicorns_dict['whiskey'] = [len(whiskey), self.sort_by_price(whiskey)]
-        unicorns_dict['rum'] = [len(rum), self.sort_by_price(rum)]
-        unicorns_dict['agave'] = [len(agave), self.sort_by_price(agave)]
-        unicorns_dict['gin'] = [len(gin), self.sort_by_price(gin)]
-        top_prices = self.sort_by_price(fancy)
-        unicorns_dict['fancy'] = [len(fancy), top_prices]
-        unicorns_dict['max'] = top_prices[:10]
-
-        # adding in data for stores; new state, new variable name
-        updated_unicorns_dict = self.get_stores_data(top_store, unicorns_dict, most_bottles_store_id, top_store_contents)
-
-        context['unicorns'] = updated_unicorns_dict
-
-        return render_to_response('index.html', context, context_instance=RequestContext(request))
+        return render_to_response('search_results.html', context, context_instance=RequestContext(request))
