7fc667023626b45e980fddde23b1beb018082dfb
Jacob Sanders <j.q.sanders@gmail.com>
2016-05-12 14:28:46 -0400

    set up management command for crontab

--- main/management/commands/cron_job.py
+++ main/management/commands/cron_job.py
@@ -1,14 +1,16 @@
 from django.core.management.base import BaseCommand
 import os, sys
 
-sys.path.append("../../scripts")
+sys.path.append("../../..")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-from main.models import Unicorn
+from main.models import Unicorns, Stores
+from main.scripts import pdf_multi_product_getter as pg
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         help = "Runs the daily PLCB scraper."
-        # function call here.
+
+        pg.hunt_unicorns()
