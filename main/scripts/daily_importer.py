772c147d56886ad26e905ad87ed81845605d6567
Jacob Sanders <j.q.sanders@gmail.com>
2016-05-20 12:26:07 -0400

    making sure a previously unknown store gets added

--- main/scripts/daily_importer.py
+++ main/scripts/daily_importer.py
@@ -54,14 +54,14 @@ def get_lat_long(address):
     r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?', params=p)
     latitude = r.json()['results'][0]['geometry']['location']['lat']
     longitude = r.json()['results'][0]['geometry']['location']['lng']
-    return latitude, longtitude
+    return latitude, longitude
 
 
 def make_new_store(unicorn):
     '''
     sometimes the PLCB adds inventory for a new store before it opens. we still
     want to capture that. in case of a store_id not in our database, we'll
-    create the object
+    create the object as a last resort if we don't have it
 
     args:
     unicorn JSON to hand off to the Store-building function
@@ -72,7 +72,7 @@ def make_new_store(unicorn):
     store = Store()
     tree = get_store_tree(unicorn)
     store_type = tree.xpath('//*/table[3]/tr[4]/*/font/text()')
-    hours = [s.strip() for s in new_tree.xpath('/html/body/table[3]/tr[4]/td[3]/table/tr/td/font/text()')]
+    hours = [s.strip() for s in tree.xpath('/html/body/table[3]/tr[4]/td[3]/table/tr/td/font/text()')]
     address_and_phone = [s.strip() for s in tree.xpath('//*/table[3]/tr[4]/td[2]/text()')]
     store.store_id = tree.xpath('//*/tr[4]/td/text()')[0].strip()
 
@@ -85,8 +85,10 @@ def make_new_store(unicorn):
         else:
             entry += ' '
             address += entry
-            address = address.strip()
-            store.address = address
+            print address
+        address = address.strip()
+        print address
+        store.address = address
 
     store.latitude, store.longitude = get_lat_long(address)
     if store_type and 'Premium' in store_type[1]:
@@ -95,7 +97,7 @@ def make_new_store(unicorn):
         store.store_type = 'Regular-ass store'
     store.hours = [{day: hours_range} for (day, hours_range) in format_hours(hours)]
     store_data_date = datetime.date.today().strftime('%Y-%m-%d')
-    return store
+    store.save()
 
 
 def import_unicorns(filepath):
@@ -116,39 +118,42 @@ def import_unicorns(filepath):
         u.on_sale = unicorn['on_sale']
         u.on_sale_price = unicorn['on_sale']
         u.scrape_date = unicorn['scrape_date']
-        if not Store.objects.filter(store_id=str(unicorn['store_id']), store_data_date__lte='2016-05-15').exists():
-            print 'making a new store ....'
-            u.store = make_new_store(unicorn)
-        else:
+        # this is super ugly. for now it just needs to work
+        try:
+            u.store = Store.objects.get(store_id=str(unicorn['store_id']), store_data_date='2016-04-10')
+        except Store.DoesNotExist:
             try:
                 u.store = Store.objects.get(store_id=str(unicorn['store_id']), store_data_date='2016-05-15')
             except Store.DoesNotExist:
-                u.store = Store.objects.get(store_id=str(unicorn['store_id']), store_data_date='2016-04-10')
-            except Store.DoesNotExist:
-                print unicorn, e
+                try:
+                    u.store = Store.objects.get(store_id=str(unicorn['store_id']), store_data_date__lte=datetime.date.today().strftime('%Y-%m-%d'))
+                except Store.DoesNotExist:
+                    print 'making a new store ....'
+                    make_new_store(unicorn)
+                    u.store = Store.objects.get(store_id=unicorn['store_id'], store_data_date=datetime.date.today().strftime('%Y-%m-%d'))
 
         u.save()
 
 
-def import_stores(filepath):
-    '''
-    a function to use infrequently, whenever we need to update stores data
-    '''
-    Store.objects.all().delete()
-
-    j = json.load(open(filepath, 'r'))
-    for store in j:
-        s = Store.objects.get_or_create(store_id=store['id'],
-                                        address=store['address'],
-                                        hours=store['hours'],
-                                        latitude=store['latitude'],
-                                        longitude=store['longitude'],
-                                        phone=store['phone'],
-                                        store_type=store['store_type'])
+# def import_stores(filepath):
+#     '''
+#     a function to use infrequently, whenever we need to update stores data
+#     '''
+#     Store.objects.all().delete()
+#
+#     j = json.load(open(filepath, 'r'))
+#     for store in j:
+#         s = Store.objects.get_or_create(store_id=store['id'],
+#                                         address=store['address'],
+#                                         hours=store['hours'],
+#                                         latitude=store['latitude'],
+#                                         longitude=store['longitude'],
+#                                         phone=store['phone'],
+#                                         store_type=store['store_type'])
 
 
 def migrate():
-    import_stores()
+    # import_stores()
     import_unicorns()
 
 
