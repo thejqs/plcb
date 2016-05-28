863350537cb969610c703f370f0e34ad4124a070
Jacob Sanders <j.q.sanders@gmail.com>
2016-05-27 23:04:43 -0400

    new urls for new views

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from main.views import UnicornView
+from main.views import AllUnicornsView, TopStoresView, FancyView
 
 admin.autodiscover()
 
 
 urlpatterns = [
-    url(r'^$', UnicornView.as_view(), name='unicorns'),
+    url(r'^$', AllUnicornsView.as_view(), name='unicorns'),
+    url(r'^top-stores/$', TopStoresView.as_view(), name='stores'),
+    url(r'^fancy/$', FancyView.as_view(), name='fancy'),
 ]
