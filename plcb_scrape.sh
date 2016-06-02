0fb5c9908d6244699f1cbc870b5f9e59cd84c57f
Jacob Sanders <j.q.sanders@gmail.com>
2016-06-02 12:09:43 -0400

    properly ordering stdout writes

--- plcb_scrape.sh
+++ plcb_scrape.sh
@@ -12,7 +12,7 @@ completes and dumps its data.
 /sites/virtualenvs/plcb/bin/python /sites/projects/plcb/manage.py cron_job >> /sites/projects/plcb/cronlog.txt
 
 printf "restarting apache and varnish ....\n" >> /sites/projects/plcb/cronlog.txt
-printf "_%.0s\n" {1..30} >> /sites/projects/plcb/cronlog.txt
 service apache2 restart
 service varnish restart
 printf "done." >> /sites/projects/plcb/cronlog.txt
+printf "_%.0s\n" {1..30} >> /sites/projects/plcb/cronlog.txt
