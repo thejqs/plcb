0ed69a06da23addc3f2825ab0314b4aff2a0cf3a
Jacob Sanders <j.q.sanders@gmail.com>
2016-06-02 11:44:38 -0400

    added newlines to stdout output

--- plcb_scrape.sh
+++ plcb_scrape.sh
@@ -2,9 +2,8 @@
 
 /sites/virtualenvs/plcb/bin/python /sites/projects/plcb/manage.py cron_job >> /sites/projects/plcb/cronlog.txt
 
-printf "restarting apache and varnish ...." >> /sites/projects/plcb/cronlog.txt
-printf "_%.0s" {1..30} >> /sites/projects/plcb/cronlog.txt
+printf "restarting apache and varnish ....\n" >> /sites/projects/plcb/cronlog.txt
+printf "_%.0s\n" {1..30} >> /sites/projects/plcb/cronlog.txt
 service apache2 restart
 service varnish restart
 printf "done." >> /sites/projects/plcb/cronlog.txt
-
