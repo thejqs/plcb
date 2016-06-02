46a5d6ccf103106c3a72eb71b8ae8b22b226b38c
Jacob Sanders <j.q.sanders@gmail.com>
2016-06-02 11:55:49 -0400

    added top docstring

--- plcb_scrape.sh
+++ plcb_scrape.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+'''
+a helper script to make sure the server process updates to account for
+new data and also that the cache refreshes at the right time --
+but only after the scraper completes and exits, reliquishing memory
+'''
+
 /sites/virtualenvs/plcb/bin/python /sites/projects/plcb/manage.py cron_job >> /sites/projects/plcb/cronlog.txt
 
 printf "restarting apache and varnish ....\n" >> /sites/projects/plcb/cronlog.txt
