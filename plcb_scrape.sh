24127b8342138350562cfbb39a5b5d9766b504d6
Jacob Sanders <j.q.sanders@gmail.com>
2016-06-03 10:23:04 -0400

    big honkin\' docstring in a .sh script. the hell was I thinking

--- plcb_scrape.sh
+++ plcb_scrape.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env zsh
 
-'''
-a helper script to make sure the server process updates to account for
-new data and also that the cache refreshes at the right time --
-but only after the scraper completes and exits, reliquishing memory. crontab
-can run this file instead of the scraper itself and then there is no reason
-to try to, say, track some state change elsewhere to figure out when the scraper
-completes and dumps its data.
-'''
-
 /sites/virtualenvs/plcb/bin/python /sites/projects/plcb/manage.py cron_job >> /sites/projects/plcb/cronlog.txt
 
 printf "restarting apache and varnish ....\n" >> /sites/projects/plcb/cronlog.txt
